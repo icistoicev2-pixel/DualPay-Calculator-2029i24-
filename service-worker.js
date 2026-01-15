69bb791529c717299b870f35f1b4e113efea41c4
icistoicev2-pixel <icistoicev2@gmail.com>
2026-01-15 18:32:01 +0200

    Refactor service worker event listeners and caching

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,31 @@
 const CACHE_NAME = 'dualpay-v1';
-
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (e) => {
+  // Активира новия Service Worker веднага
   self.skipWaiting();
-  event.waitUntil(
+  e.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
+      .then((cache) => cache.addAll(ASSETS))
+      .catch(err => console.error('Грешка при кеширане:', err))
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', (e) => {
+  // Поема контрола над страницата веднага
+  e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
+      .catch(err => console.error('Грешка при fetch:', err))
   );
 });
