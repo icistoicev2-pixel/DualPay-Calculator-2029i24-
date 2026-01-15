2c47684f33261ab6dfcd61587cd43432e5bacfa7
icistoicev2-pixel <icistoicev2@gmail.com>
2026-01-15 17:41:44 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'dualpay-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;600;700&family=Montserrat:wght@400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
